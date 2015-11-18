e13c365be166aabc40460190a606c77865a9a610
Danny <Danny@Polandas-Mini.fios-router.home>
2015-11-17 23:11:06 -0500

    Added initial functions

--- src/merge_sort.cpp
+++ src/merge_sort.cpp
@@ -8,14 +8,41 @@
 // Namespace
 using namespace std;
 
+// Print the array to screen
+void print_array(int *arr, int size) {
+    // For each element in the array, print to cout
+    for (int idx = 0; idx < size; idx++) {
+        cout << arr[idx];
+        if (idx != size-1) {
+            cout << ", ";
+        }
+        else {
+            cout << '\n';
+        }
+    }
+}
+
+
+// Merge sort function
+void merge_sort(int *arr, int size) {
+
+}
+
+
 // Main function
 int main() {
 
-    // Declare variables
-    int unsorted_array[10];
+    // Declare/init variables
+    int unsorted_array[] = { 5, 4, 1, 8, 7, 2, 6, 3, 4, 1 };
 
-    // Init variables
-    unsorted_array = {5, 4, 1, 8, 7, 2, 6, 3};
+    // Get length of array
+    int array_size = sizeof(unsorted_array) / sizeof(*unsorted_array);
+
+    // Print unsorted values
+    cout << "Unsorted array elements:\n";
+    print_array(unsorted_array, array_size);
+
+    //
 
     return 0;
 }
