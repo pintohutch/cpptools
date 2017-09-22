db8b1a0c34147e624e9d8661d75a009a9e32d5ac
Daniel Clark <daniel.clark@liveramp.com>
2017-09-21 23:10:15 -0400

    Moved implementation into header per google style

--- include/cpptools/linkedlist.h
+++ include/cpptools/linkedlist.h
@@ -6,19 +6,17 @@
 #ifndef CPPTOOLS_LINKEDLIST_H_
 #define CPPTOOLS_LINKEDLIST_H_
 
+#include <stddef.h>
+
 namespace cpptools {
-template <class T>
+template <typename T>
 struct Node {
   T value;
   Node<T>* next;
 };
 
-template <class T>
+template <typename T>
 class LinkedList {
-  private:
-    Node<T>* head_; // Pop() would return.
-    int size_;
-
   public:
     // Default constructor - creates head node
     LinkedList();
@@ -26,7 +24,43 @@ class LinkedList {
     T Pop();
     void Push(T value);
     int Size();
+
+  private:
+    Node<T>* head_;
+    int size_;
 };
+
+template <typename T>
+LinkedList<T>::LinkedList() {
+  head_ = NULL;
+  size_ = 0;
+}
+
+template <typename T>
+T LinkedList<T>::Pop() {
+  if (head_ != NULL) {
+    T val = head_->value;
+    head_ = head_->next;
+    size_--;
+    return val;
+  } else {
+    throw "Cannot pop an empty list!";
+  }
+}
+
+template <typename T>
+void LinkedList<T>::Push(T value) {
+  Node<T>* new_head = new Node<T>;//{T, head_};
+  new_head->value = value;
+  new_head->next = head_;
+  head_ = new_head;
+  size_++;
+}
+
+template <typename T>
+int LinkedList<T>::Size() {
+  return size_;
+}
 }
 
 #endif //CPPTOOLS_LINKEDLIST_H
