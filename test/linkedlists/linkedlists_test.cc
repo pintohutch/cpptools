3a4181727a179835745fef1fe3b8b2b59e6eed4c
Daniel Clark <daniel.clark@liveramp.com>
2018-01-31 21:18:04 -0500

    wip

--- test/linkedlists/linkedlists_test.cc
+++ test/linkedlists/linkedlists_test.cc
@@ -27,18 +27,18 @@ TEST_F(LinkedListTest, LValueConstructor) {
 }
 
 TEST_F(LinkedListTest, RValueConstructor) {
-  cpptools::LinkedList<int> ll{0};
+  cpptools::LinkedList<int> ll{1};
   EXPECT_EQ(ll.size(), 1);
 }
 
 TEST_F(LinkedListTest, CopyConstructor) {
-  cpptools::LinkedList<int> ll1{0};
-  ll1.push_front(1);
+  cpptools::LinkedList<int> ll1{1};
+  ll1.push_front(2);
   cpptools::LinkedList<int> ll2 = ll1;
   EXPECT_EQ(ll1.size(), 2);
   EXPECT_EQ(ll2.size(), 2);
+  EXPECT_EQ(ll2.pop_front(), 2);
   EXPECT_EQ(ll2.pop_front(), 1);
-  EXPECT_EQ(ll2.pop_front(), 0);
   EXPECT_EQ(ll2.size(), 0);
   EXPECT_EQ(ll1.size(), 2);
 }
@@ -52,9 +52,20 @@ TEST_F(LinkedListTest, MoveConstructor) {
 }
 
 TEST_F(LinkedListTest, CopyAssignment) {
-
+  cpptools::LinkedList<int> ll1{1};
+  cpptools::LinkedList<int> ll2;
+  ll2 = ll1;
+  EXPECT_EQ(ll2.size(), 1);
+  EXPECT_EQ(ll2.pop_front(), 1);
+  EXPECT_EQ(ll1.size(), 1);
+  EXPECT_EQ(ll1.pop_front(), 1);
 }
 
 TEST_F(LinkedListTest, MoveAssignment) {
-
+  cpptools::LinkedList<int> ll1{1};
+  cpptools::LinkedList<int> ll2;
+  ll2 = std::move(ll1);
+  EXPECT_EQ(ll1.size(), 0);
+  EXPECT_EQ(ll2.size(), 1);
+  EXPECT_EQ(ll2.pop_front(), 1);
 }
